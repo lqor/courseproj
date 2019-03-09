0616f058e1ed9eae7e7cd1772a15e64a61691a49
lqor <36489255+lqor@users.noreply.github.com>
2019-03-09 15:04:46 +0100

    Update sendDataScript.js

--- web/js/sendDataScript.js
+++ web/js/sendDataScript.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
         var textValue = $('#textField').val();
         $.ajax({
-            url: '/classHandler',
+            url: '/classHandler',  /* zu dieser Adresse soll einen Servlet gemappt werden. Er ist fürBearbeitung eines Requestes zuständig*/
             method: 'POST',
             data: {"textFieldParam": textValue}, /* "textFieldParam" wird als Parameter in Servlet ansprechbar sein*/
 

13cd9e82d2949eafc7e5b425a30ba8d36e771a7f
lqor <36489255+lqor@users.noreply.github.com>
2019-03-10 21:16:51 +0100

    Delete PatternClassForEx1.java

--- src/Logic/PatternClasses/PatternClassForEx1.java
+++
@@ -1,9 +0,0 @@
-package Logic.PatternClasses;
-
-/**Anhand von dieser Klase wird entschieden, ob die vom Benutzer eingegebene Klasse den Anforderungen entspricht
- * Alle Attribute werden rausgenommen und mit eingegebene verglichen
- * */
-public class PatternClassForEx1 {
-    private String name;
-    private int age;
-}
